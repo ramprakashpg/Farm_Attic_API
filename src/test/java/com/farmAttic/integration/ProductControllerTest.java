1bbdb68700b93222b7836bca67c18b256b81b144
SahitiPriyaA <sahitipriya.a@thoughtworks.com>
2023-06-26 15:11:06 +0530

    Add integration tc

--- src/test/java/com/farmAttic/integration/ProductControllerTest.java
+++ src/test/java/com/farmAttic/integration/ProductControllerTest.java
@@ -8,16 +8,15 @@ import com.farmAttic.repositories.UserRepository;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.client.HttpClient;
 import io.micronaut.http.client.annotation.Client;
-import io.micronaut.security.authentication.Authentication;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import jakarta.inject.Inject;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 @MicronautTest
 class ProductControllerTest {
@@ -69,6 +68,98 @@ class ProductControllerTest {
 
         String expectedResponse = "[{\"productId\":\"" + product.getProductId() + "\",\"userId\":\"" + user.getUserId() + "\",\"productName\":\"product\",\"productDescription\":\"description\",\"quantity\":12,\"price\":12}]";
         String actualResponse = client.toBlocking().retrieve(HttpRequest.GET("v1/product").bearerAuth("anything"), String.class);
+
+        Assertions.assertEquals(expectedResponse, actualResponse);
+
+    }
+
+    @Test
+    void shouldGetAllUserProducts() {
+        User user = new User();
+        user.setEmail("dummy@test.com");
+        user.setFirstName("Sahiti");
+        user.setLastName("Priya");
+        User user1 = new User();
+        user.setEmail("dummy@test123.com");
+        user.setFirstName("Sahiti");
+        user.setLastName("Priya");
+        Product product = Product.builder().productName("product").productDescription("description").quantity(12).price(12).user(user).build();
+        Product product1 = Product.builder().productName("product").productDescription("description").quantity(12).price(12).user(user1).build();
+
+        userRepository.save(user);
+        userRepository.save(user1);
+        productRepository.save(product);
+        productRepository.save(product1);
+        entityManager.getTransaction().commit();
+        entityManager.clear();
+        entityManager.close();
+
+        String expectedResponse = "[{\"productId\":\"" + product.getProductId() + "\",\"userId\":\"" + user.getUserId() + "\",\"productName\":\"product\",\"productDescription\":\"description\",\"quantity\":12,\"price\":12}]";
+        String actualResponse = client.toBlocking().retrieve(HttpRequest.GET("v1/product"+"/"+user.getUserId()).bearerAuth("anything"), String.class);
+
+        Assertions.assertEquals(expectedResponse, actualResponse);
+
+    }
+
+    @Test
+    void shouldCreateProduct() {
+        User user = new User();
+        user.setEmail("dummy@test.com");
+        user.setFirstName("Sahiti");
+        user.setLastName("Priya");
+
+        userRepository.save(user);
+        entityManager.getTransaction().commit();
+        entityManager.clear();
+        entityManager.close();
+
+        String dataRequest="{\n" +
+                "    \"userId\":\""+user.getUserId()+"\",\n" +
+                "    \"productDescription\": \"sahiti\",\n" +
+                "    \"productName\": \"priya Addepalli\",\n" +
+                "    \"quantity\": 122,\n" +
+                "    \"price\":225,\n" +
+                "    \"imageList\": [[-1, -40, -1, -32, 0, 16, 74, 70, 73]]\n" +
+                "}";
+
+        String actualResponse = client.toBlocking().retrieve(HttpRequest.POST("v1/product", dataRequest)
+                .bearerAuth("anything"));
+
+        List<Product> product=productRepository.findAll();
+
+        String expectedResponse="{\"productId\":\""+product.get(0).getProductId()+"\",\"userId\":\""+user.getUserId()+"\",\"productName\":\"priya Addepalli\",\"productDescription\":\"sahiti\",\"quantity\":122,\"price\":225,\"imageList\":[\"/9j/4AAQSkZJ\"]}";
+
+        Assertions.assertEquals(expectedResponse, actualResponse);
+
+    }
+
+    @Test
+    void shouldUpdateProduct() {
+        User user = new User();
+        user.setEmail("dummy@test.com");
+        user.setFirstName("Sahiti");
+        user.setLastName("Priya");
+        Product product = Product.builder().productName("product").productDescription("description").quantity(12).price(12).user(user).build();
+
+        userRepository.save(user);
+        productRepository.save(product);
+        entityManager.getTransaction().commit();
+        entityManager.clear();
+        entityManager.close();
+
+        String dataRequest="{\n" +
+                "    \"userId\":\""+user.getUserId()+"\",\n" +
+                "    \"productDescription\": \"sahiti\",\n" +
+                "    \"productName\": \"priya Addepalli\",\n" +
+                "    \"quantity\": 122,\n" +
+                "    \"price\":225,\n" +
+                "    \"imageList\": [[-1, -40, -1, -32, 0, 16, 74, 70, 73]]\n" +
+                "}";
+
+        String actualResponse = client.toBlocking().retrieve(HttpRequest.PUT("v1/product/"+product.getProductId(), dataRequest)
+                .bearerAuth("anything"));
+
+        String expectedResponse="{\"productId\":\""+product.getProductId()+"\",\"userId\":\""+user.getUserId()+"\",\"productName\":\"priya Addepalli\",\"productDescription\":\"sahiti\",\"quantity\":122,\"price\":225,\"imageList\":[\"/9j/4AAQSkZJ\"]}";
 
         Assertions.assertEquals(expectedResponse, actualResponse);
 
